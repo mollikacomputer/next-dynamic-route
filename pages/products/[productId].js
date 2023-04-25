a83811fb6f0806d60805cafd0a58384afd943356
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-25 08:00:55 +0600

    map function contidionally add classname col- row-reverse

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,51 +1,51 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const productDetails = ({product }) => {
-
+const productDetails = ({ product }) => {
   return (
     <div>
       <h2 className="text-3xl my-8">
         {product.id} {product.title}
-      <p>{product.body}</p>
-      <button className="btn">
-      <Link href={'/products'}> Go to product pages</Link>
-      </button>
+        <Image src={product.image} width={350} height={250} />
       </h2>
-      <hr/>
+      <p className="text-justify" >{product.body}</p>
+      <button className="btn">
+          <Link href={"/products"}> Go to product pages</Link>
+        </button>
+      <hr />
     </div>
   );
 };
 export default productDetails;
 
 export const getStaticPaths = async () => {
-    return {
-      paths:[
-        {
-            params:{productId:'1'},
-        },
-        {
-            params:{productId:'2'},
-        },
-        {
-            params:{productId:'3'},
-        },
+  return {
+    paths: [
+      {
+        params: { productId: "1" },
+      },
+      {
+        params: { productId: "2" },
+      },
+      {
+        params: { productId: "3" },
+      },
     ],
-      fallback: false,
-    };
+    fallback: false,
   };
+};
 
 export const getStaticProps = async (context) => {
-    const { params } = context;
-    const response = await fetch(
-      `http://localhost:4000/products/${params?.productId}`
-    );
-    const data = await response.json();
-  
-    return {
-      props: {
-        product: data,
-      },
-    };
-  };
+  const { params } = context;
+  const response = await fetch(
+    `http://localhost:4000/products/${params?.productId}`
+  );
+  const data = await response.json();
 
+  return {
+    props: {
+      product: data,
+    },
+  };
+};
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,22 +1,53 @@
+import Image from "next/image";
 import Link from "next/link";
+
 const productList = ({ products }) => {
   return (
-    <div>
-      <h2 className="text-2xl">Post Length:{products.length} </h2>
-      {products.map((product) => {
-        return (
-          <div key={product?.id}>
-            <Link href={`/products/${product?.id}`}>
-              <h2 className="text-3xl">
-                {product.id} {product.title}
-              </h2>
-            </Link>
-            <p>{product.body}</p>
-            <hr />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <section>
+        {products.map((product) => {
+          return (
+            <div key={product.id} className="hero min-h-screen bg-base-100">
+              <div className={`hero-content flex-col lg:${product.id == 2 ? "flex-row" : "flex-row-reverse"}`
+              }>
+                <Link href={`/products/${product?.id}`}>
+                  <Image
+                    className="max-w-sm rounded-lg shadow-2xl"
+                    src={product.image}
+                    width={350}
+                    height={250}
+                  />
+                </Link>
+                <div>
+                  <Link href={`/products/${product?.id}`}>
+                    <h1 className="text-5xl font-bold"> {product.title} </h1>
+                  </Link>
+                  <p className="py-6">{product.body}</p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </>
+    // <div>
+    //   <h2 className="text-2xl">Post Length:{products.length} </h2>
+    //   {products.map((product) => {
+    //     return (
+    //       <div key={product?.id}>
+    //         <Link href={`/products/${product?.id}`}>
+    //           <h2 className="text-3xl">
+    //             {product.id} {product.title}
+    //           </h2>
+    //           <Image src={product.image} width={350} height={250} />
+    //         </Link>
+    //         <p>{product.body}</p>
+    //         <hr />
+    //       </div>
+    //     );
+    //   })}
+    // </div>
   );
 };
 export default productList;
